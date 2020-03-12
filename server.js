8ad1e4cb305d86a6de3daaf5df2e3f9374383594
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-12 13:00:43 -0300

    configurado o setup do app - arquivo server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+
+
+//Congidurar setup da App:
+
+//Chamadas dos pacotes
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+//Configuração da variável app para usar  o 'bodyParse()'
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+//Definindo a porta onde será executada a nossa api:
+const port = process.env.port || 8000
+
+//Criando uma instancia das rotas via express
+const router = express.Router()
+
+//Rota de exemplo:
+router.get('/', (req, res)=>{
+    res.json({ message: 'Benvindo a nossa loja XYZ...'})
+})
+
+//definindo um padrão das rotas prefixadas: '/api'
+app.use('/api/', router)
+
+//Iniciando a Aplicação (servidor)
+app.listen(port, ()=>{
+    console.log("servidor funcionando na porta: " + port)
+})
